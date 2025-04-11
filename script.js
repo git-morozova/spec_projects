1b2d28ce5e61681ec738122cf662e96c9970b15d
unknown <morozova.freelance@yandex.ru>
2025-04-11 17:47:44 +0300

    main video

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Функция для перемещения видео на указанную временную метку
 function jumpToTime(seconds) {
-    var video = document.getElementById("myvideo");
+    const video = document.querySelector('#video');
     video.currentTime = seconds;
+
+    const anchor = document.querySelector('#a-video'); 
+    anchor.scrollIntoView({ behavior: 'smooth' });
 }
 
-
-
-
+// Кнопка разворачивания скрытого текста
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const expandableBlock = document.getElementById('expandableBlock');
+    
+    if (expandableBlock.classList.contains('hidden')) {
+        expandableBlock.classList.remove('hidden');
+        expandableBlock.classList.add('visible');        
+        this.style.display = 'none'; 
+    }
+});
 
