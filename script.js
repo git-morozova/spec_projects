aa3bb63c48198b8e12a3701a7724d0e182d3a289
unknown <morozova.freelance@yandex.ru>
2025-04-11 10:30:54 +0300

    top

---
+++ script.js
@@ -0,0 +1,10 @@
+// Функция для перемещения видео на указанную временную метку
+function jumpToTime(seconds) {
+    var video = document.getElementById("myvideo");
+    video.currentTime = seconds;
+}
+
+
+
+
+
