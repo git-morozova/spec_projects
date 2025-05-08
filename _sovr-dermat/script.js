abfad7051a319c91e6633071dc92071a99d1516f
unknown <morozova.freelance@yandex.ru>
2025-05-08 20:04:50 +0300

    sovr dermat

--- _sovr-dermat/script.js
+++ _sovr-dermat/script.js
@@ -35,6 +35,44 @@ if (hideButton) {
     });
 }
 
+// Кнопки разворачивания и сворачивания скрытого текста2
+const showButton2 = document.getElementById('showButton2');
+if (showButton2) {
+    showButton2.addEventListener('click', function () {
+        const expandableBlock2 = document.getElementById('expandableBlock2');
+        if (expandableBlock2 && expandableBlock2.classList.contains('project__hidden')) {
+            expandableBlock2.classList.remove('project__hidden');
+            expandableBlock2.classList.add('project__visible');
+        
+            const hideButton2 = document.getElementById('hideButton2');
+            if (hideButton2) {
+                hideButton2.classList.remove('project__hidden');
+                hideButton2.classList.add('project__visible');            
+                showButton2.classList.add('project__hidden');
+            }
+        }
+    });
+}
+
+const hideButton2 = document.getElementById('hideButton2');
+if (hideButton2) {
+    hideButton2.addEventListener('click', function () {
+        const expandableBlock2 = document.getElementById('expandableBlock2');
+        if (expandableBlock2 && expandableBlock2.classList.contains('project__visible')) {
+            expandableBlock2.classList.remove('project__visible');
+            expandableBlock2.classList.add('project__hidden');
+        
+            const showButton2 = document.getElementById('showButton2');
+            if (showButton2) {
+                showButton2.classList.remove('project__hidden');
+                showButton2.classList.add('project__visible');            
+                hideButton2.classList.add('project__hidden');
+            }
+        }
+    });
+}
+
+
 // ЭКСПЕРТЫ - СЛАЙДЕР
 let timer;
 
